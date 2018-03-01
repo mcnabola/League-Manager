2483bbcf70a281cf4711fc4499140a390faa3ae9
seanmorrissey98 <35046602+seanmorrissey98@users.noreply.github.com>
2018-03-01 16:34:03 +0000

    Update League_Manager.java

--- League_Manager.java
+++ League_Manager.java
@@ -102,7 +102,7 @@ public class League_Manager
 	 public static void writeFile(String input, String fileName) 
 	 {
          try (Writer writer = new BufferedWriter(new OutputStreamWriter(
-         new FileOutputStream(filename), "utf-8"))) 
+         new FileOutputStream(fileName), "utf-8"))) 
 		 {
              writer.write(input);
          }
@@ -152,6 +152,141 @@ public class League_Manager
 		 
 		 return line;
 	 } 
+	
+/* seans code so far
+import java.io.*;
+import javax.swing.*;
+import java.util.*;
+
+public class League
+{
+	private static int currentAdminNo=1;
+	private static int leagueNo=1;
+	final static String leagueFile="league.txt";
+	final static String adminFile="administrator.txt";
+	
+	public static void main(String [] args)
+	{
+	createNewLeague();
+    addTeamsToLeague();
+	}
+	
+		 
+	public static void writeFile(String input, String fileName) 
+	 {
+         try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+         new FileOutputStream(fileName), "utf-8"))) 
+		 {
+             writer.write(input);
+         }
+         catch(Exception e)
+         {} 
+         
+	 }
+	 
+	 public static String menuBox(String Options)
+	 {
+		 String input="";
+		 input=JOptionPane.showInputDialog(null,Options);
+		 return input;
+	 }
+	 
+	 public static int menuBoxInt(String options)
+	{
+		String text = JOptionPane.showInputDialog(null, options);
+		int x = Integer.parseInt(text);
+		return x;
+	}
+	
+	public static void createNewLeague()
+	{
+		String leagueName=""; String leagueFileInput="";
+		leagueName=menuBox("Enter your league name:");
+		leagueFileInput=currentAdminNo+","+leagueName+","+leagueNo;
+		writeFile(leagueFileInput,leagueFile);
+	    leagueNo++;
+	}
+	
+	public static int getNumberOfLeaguesMade()
+	{
+		boolean sameAdmin=true;
+		boolean found=false;
+		int currentAdminPostion=0;
+		int temp=0;
+		int numberOfLeagues=0;
+		String [] arrayOfDetails=readFile(leagueFile).split(",");
+		for (int i=0;i<arrayOfDetails.length&&found==false;i=i+3)
+		{
+			temp=Integer.parseInt(arrayOfDetails[i]);
+			if(temp==currentAdminNo)
+			{
+				found=true;
+				currentAdminPostion=i;
+			}
+		}
+		for(int i=currentAdminPostion;i<arrayOfDetails.length && sameAdmin==true;i=i+3)
+		{
+			if(currentAdminNo!=Integer.parseInt(arrayOfDetails[i]))
+			{
+				sameAdmin=false;
+			}
+			else
+			{
+				numberOfLeagues=Integer.parseInt(arrayOfDetails[i+2]);
+			}
+		}
+		return numberOfLeagues;
+	}
+	
+	public static String readFile(String textFile)
+	{
+		String fileText="";
+		try
+		{
+		FileReader reader=new FileReader(textFile);
+		Scanner in=new Scanner(reader);
+		while(in.hasNext())
+		{
+			fileText=in.nextLine();
+			fileText=fileText+",";
+		}
+		in.close();
+		reader.close();
+		}
+		catch(Exception E)
+		{}
+		return fileText;
+	}
+	
+	public static void outputBoxs(String output)
+	{
+	     JOptionPane.showMessageDialog(null, output);	
+	}
+	
+	public static void addTeamsToLeague()
+	{
+		String info=""; String teamName=""; String teamFileInfo=""; String teamFileName="";
+		int whichLeague=menuBoxInt("Enter which league number to add teams/players to:");
+		teamFileName=whichLeague+"_"+"participants.txt";
+		if(getNumberOfLeaguesMade()<whichLeague)
+		{
+			outputBoxs("This league does not exist.");
+		}
+		else
+		{
+			int numberOfTeams=menuBoxInt("Enter the amount of teams/players:");
+			for(int i=0;i<numberOfTeams;i++)
+			{
+				info="enter team/player number:"+(i+1);
+				teamName=menuBox(info);
+				teamFileInfo=(i+1)+","+teamName;
+				writeFile(teamFileInfo,teamFileName);
+			}
+		}
+	}
+
+}
+*/
 
 }
 
